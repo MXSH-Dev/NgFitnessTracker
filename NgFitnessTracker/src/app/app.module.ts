b86d325ea3a5b6c92427ed1d542f67a483591d97
mxsh-dev <x597653559@gmail.com>
2020-11-18 14:44:20 -0700

    generated skeletion

---
+++ NgFitnessTracker/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { MaterialModule } from './material/material.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { TrainingComponent } from './components/training/training.component';
+import { CurrentTrainingComponent } from './components/training/current-training/current-training.component';
+import { NewTrainingComponent } from './components/training/new-training/new-training.component';
+import { PastTrainingComponent } from './components/training/past-training/past-training.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    TrainingComponent,
+    CurrentTrainingComponent,
+    NewTrainingComponent,
+    PastTrainingComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
